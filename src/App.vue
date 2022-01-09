6a01dae491f466f2b221e4523480eb4a4f822668
董浩 <barry.dong@morefun.me>
2022-01-09 16:56:59 +0800

    图片重命名 & 实现图片瀑布流

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Header />
+    <Main />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
@@ -13,13 +15,14 @@ export default {
     return {
     }
   },
-  components: { Header }
+  components: { Header, Main }
 }
 </script>
 
 <style>
 * { margin: 0; padding: 0; box-sizing: border-box; }
 ul, ol, li { list-style: none; }
+img { max-width: 100%; }
 
 .icon {
     width: 1em; height: 1em;
@@ -31,7 +34,7 @@ ul, ol, li { list-style: none; }
 #app {
   width: 100vw;
   height: 100vh;
-  background: url("./assets/photos/IMG_9546.jpg") no-repeat center top;
+  background: url("./assets/photos/93.jpg") no-repeat center top;
   background-size: 100% 100%;
   overflow: hidden;
   border: 1px solid red;
---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+    <main>
+
+        <!-- 图片 -->
+        <section class="photo">
+            <img v-for="(item, index) in imgList" :key="index" :src="item.img" alt="">
+        </section>
+
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return {
+            imgList: []
+        }
+    },
+    created() {
+        for (let i = 1; i <= 100; i ++) {
+            this.imgList.push({ img: require(`../assets/photos/${i}.jpg`) })
+        }
+    }
+}
+</script>
+
+<style scoped>
+main {
+    /* border: 2px solid green; */
+    height: calc(100vh - 90px - 100px);
+    overflow: hidden auto;
+    margin-top: 20px;
+}
+
+/* 图片 */
+.photo {
+    column-count: 2;
+    column-gap: 6px;
+}
+.photo > img {
+    display: block;
+    margin-top: 6px;
+}
+</style>
